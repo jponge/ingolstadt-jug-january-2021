fada1f8f90154eeb50e05b8e547ac61371c9bb26
Julien Ponge <jponge@redhat.com>
2021-01-06 21:28:24 +0100

    Simplify

--- catalog-service/src/main/java/demo/CatalogApi.java
+++ catalog-service/src/main/java/demo/CatalogApi.java
@@ -3,29 +3,29 @@ package demo;
 import io.smallrye.mutiny.Multi;
 import io.smallrye.mutiny.Uni;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.transaction.Transactional;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 
 @Path("/catalog")
 public class CatalogApi {
 
   @GET
   @Path("/list")
-  @Produces(MediaType.APPLICATION_JSON)
   public Multi<Product> list() {
     return Product.all();
   }
 
   @POST
-  @Consumes(MediaType.APPLICATION_JSON)
-  @Produces(MediaType.APPLICATION_JSON)
+  @Transactional
   public Uni<Product> register(Product product) {
     return product.persistAndFlush().replaceWith(product);
   }
 
   @GET
   @Path("/products/{name}")
-  @Produces(MediaType.APPLICATION_JSON)
   public Uni<Product> find(@PathParam("name") String name) {
     return Product.findByName(name);
   }
