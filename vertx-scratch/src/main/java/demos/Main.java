afaf09649a8d11db5855f25ae5e0039030bb810f
Julien Ponge <jponge@redhat.com>
2021-01-12 15:28:06 +0100

    Added a scratch/livecode module

---
+++ vertx-scratch/src/main/java/demos/Main.java
@@ -0,0 +1,13 @@
+package demos;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+
+  private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+  public static void main(String[] args) {
+
+  }
+}
