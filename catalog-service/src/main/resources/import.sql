4dd0f70d5e41c11ec1dfcd41d58e30f5563b00c0
Julien Ponge <jponge@redhat.com>
2021-01-12 16:14:19 +0100

    Fix catalog SQL import

---
+++ catalog-service/src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO public.product (id, name, price) VALUES (nextval('hibernate_sequence'), 'speaker', 399.90);
+INSERT INTO public.product (id, name, price) VALUES (nextval('hibernate_sequence'), 'bread', 0.95);
+INSERT INTO public.product (id, name, price) VALUES (nextval('hibernate_sequence'), 'chocolate', 4.75);
+INSERT INTO public.product (id, name, price) VALUES (nextval('hibernate_sequence'), 'mouse', 49.99);
